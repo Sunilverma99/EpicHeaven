a55dd4615750bf321c38e1619313904438fe484c
Sunil <sv784333@gmial.com>
2024-02-13 00:06:23 +0530

    changes for deploy

--- Backened/index.js
+++ Backened/index.js
@@ -1,18 +1,18 @@
 import express from "express";
 import mongoose from 'mongoose';
- import cors from "cors";
- import cookieParser from "cookie-parser";
- import signup from "./routes/auth.router.js"
- import login from "./routes/auth.router.js"
- import update from"./routes/user.router.js"
- import user from './routes/user.router.js'
- import Listings from './routes/user.router.js'
- import listing from './routes/listing.router.js'
- import dotenv from 'dotenv';
- 
- dotenv.config();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import signup from "./routes/auth.router.js";
+import login from "./routes/auth.router.js";
+import update from "./routes/user.router.js";
+import user from './routes/user.router.js';
+import Listings from './routes/user.router.js';
+import listing from './routes/listing.router.js';
+import dotenv from 'dotenv';
 
-const port = 3000;
+dotenv.config();
+
+const port = process.env.PORT || 3000;
 const app = express();
 const corsOptions = {
   origin: 'http://localhost:5173', // Replace with the actual origin of your frontend
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.json()); // Parse JSON requests
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -34,42 +34,23 @@ mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useU
     process.exit(1); // Exit the process if there's a connection error
   });
 
-app.use("/",user)
-app.use("/api",signup);
-app.use('/api',login)
-app.use('/api',update)
-app.use("/api",listing)
-app.use("/api",Listings)
+app.use("/", user);
+app.use("/api", signup);
+app.use('/api', login);
+app.use('/api', update);
+app.use("/api", listing);
+app.use("/api", Listings);
 
-
-
-app.use((err,req,res,next) => {
-      const statusCode=err.statusCode||500;
-      const message=err.message||"Internal Server Error";
-      return res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message,
-      })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Error middleware should be defined before the last listen statement
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(port, () => {
   console.log("Server is running on port", port);
